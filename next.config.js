1ab9c3f39c482dd9dbd5e98cd24b7f48e04db285
Moaz <moaz2009amen@gmail.com>
2026-04-13 23:53:16 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,28 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'ubkjrqeencijfxtpitvm.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/public/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'plus.unsplash.com',
+      },
+    ],
+  },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '2mb',
+    },
+  },
+}
 
+module.exports = nextConfig
