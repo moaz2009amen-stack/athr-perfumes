087f2a4f69df49580afa95c57cecfc0a8c07e279
Moaz <moaz2009amen@gmail.com>
2026-04-14 00:45:40 +0200

    Update layout.tsx

--- app/(store)/layout.tsx
+++ app/(store)/layout.tsx
@@ -1,1 +1,18 @@
+import { Header } from '@/components/store/Header'
+import { Footer } from '@/components/store/Footer'
 
+export default function StoreLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <>
+      <Header />
+      <main className="flex-1">
+        {children}
+      </main>
+      <Footer />
+    </>
+  )
+}
